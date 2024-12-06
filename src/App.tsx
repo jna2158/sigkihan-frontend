e6dcacd2ecbfdf64128c15f2dce6fd6c3aa21d1a
유지원 <jna215843@gmail.com>
2024-12-06 18:17:56 +0900

    feat: utils 폴더 생성

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
-import { useRef, useState, useEffect } from "react";
+import { useState } from "react";
 import { login } from "./services/authService";
+import { isValidEmail } from "./shared/utils/validEmail";
 
 function App() {
+  const [email, setEmail] = useState("");
+
   const handleLogin = () => {
     login({ email: "test@test.com", password: "test1234" })
       .then((res) => {
@@ -11,7 +14,19 @@ function App() {
         console.log(err);
       });
   };
-  return <button onClick={handleLogin}>로그인</button>;
+  return (
+    <div className="flex h-screen flex-col items-center justify-center">
+      <button onClick={handleLogin}>로그인</button>
+      <input
+        type="email"
+        onChange={(e) => setEmail(e.target.value)}
+        className="rounded-md border border-gray-300 p-2"
+      />
+      {isValidEmail(email)
+        ? "올바른 이메일입니다."
+        : "올바른 이메일이 아닙니다."}
+    </div>
+  );
 }
 
 export default App;
