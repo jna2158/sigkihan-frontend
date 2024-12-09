d50aa7ef1a9f2fa268373ee9304a6d0962cab569
유지원 <jna215843@gmail.com>
2024-12-09 12:18:47 +0900

    fix: kakao login auth url 수정

--- src/pages/Auth/components/kakaoLoginButton.tsx
+++ src/pages/Auth/components/kakaoLoginButton.tsx
@@ -2,7 +2,7 @@ const KakaoLoginButton = () => {
   const handleLogin = () => {
     const CLIENT_ID = process.env.REACT_APP_KAKAO_CLIENT_ID;
     const REDIRECT_URI = process.env.REACT_APP_KAKAO_REDIRECT_URI;
-    const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code`;
+    const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code&prompt=login`;
 
     window.location.href = KAKAO_AUTH_URL;
   };
