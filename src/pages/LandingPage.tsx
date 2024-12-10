b305881b58852e731f85733846ad7ae40441a90f
유지원 <jna215843@gmail.com>
2024-12-10 12:14:31 +0900

    fix: 모바일 화면 주소창 대응 태스트

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -39,7 +39,7 @@ export default function LandingPage() {
           alt="로고 이미지"
           className="absolute left-[2.2rem] top-[6.2rem] h-[8.9rem] w-[12.5rem]"
         />
-        <section className="absolute top-[41.8rem]">
+        <section className="absolute bottom-[8.63rem]">
           <KakaoLoginButton />
         </section>
       </div>
