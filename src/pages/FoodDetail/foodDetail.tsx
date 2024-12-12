793328078dc9feae46b7e388134bc67e518af6f7
유지원 <jna215843@gmail.com>
2024-12-12 13:11:07 +0900

    feat: 메인페이지에서 카드 하나 선택해서 상세조회

--- src/pages/FoodDetail/foodDetail.tsx
+++ src/pages/FoodDetail/foodDetail.tsx
@@ -2,9 +2,13 @@ import { useParams } from "react-router-dom";
 import apple from "../../assets/apple.svg";
 import useModalStore from "../../store/useModalStore";
 import ExpiredDateBadge from "../../components/common/expiredDateBadge";
+import useRefrigeStore from "../../store/useRefrigeStore";
+
 export default function FoodDetail() {
   const { id } = useParams();
   const { setModalOpen } = useModalStore();
+  const { foodItems } = useRefrigeStore.getState();
+  const currentFoodItem = foodItems.find((item) => item.id === Number(id));
 
   const handleEatFood = () => {
     setModalOpen("SELECT_EAT_COUNT_MODAL", true);
@@ -19,7 +23,9 @@ export default function FoodDetail() {
       <article className="flex flex-col gap-6 p-8">
         <header className="flex items-center pb-[1.9rem] pt-[2.2rem]">
           <ExpiredDateBadge />
-          <span className="ml-[0.8rem] text-[22px] text-black">빨간 사과</span>
+          <span className="ml-[0.8rem] text-[22px] text-black">
+            {currentFoodItem?.name}
+          </span>
           <div className="absolute right-[2.1rem] flex gap-4">
             <button
               onClick={() => setModalOpen("MODIFY_FOOD_MODAL", true)}
@@ -44,7 +50,7 @@ export default function FoodDetail() {
                 수량
               </span>
               <span className="font-regular ml-[3.6rem] text-[14px] text-gray-400">
-                2
+                {currentFoodItem?.quantity}
               </span>
             </p>
             <p>
@@ -52,7 +58,7 @@ export default function FoodDetail() {
                 구매 일자
               </span>
               <span className="font-regular ml-[1.9rem] text-[14px] text-gray-400">
-                2024.01.01
+                {currentFoodItem?.purchase_date?.toString()}
               </span>
             </p>
             <p>
@@ -60,15 +66,22 @@ export default function FoodDetail() {
                 유효 기간
               </span>
               <span className="font-regular ml-[1.9rem] text-[14px] text-gray-400">
-                2024.01.03
+                {currentFoodItem?.expiration_date?.toString()}
               </span>
             </p>
           </div>
         </section>
 
         <div className="flex gap-4">
-          <button className="basic-button bg-primary" onClick={handleEatFood}>맛있게 먹었어요</button>
-          <button className="basic-button bg-primary" onClick={handleDiscardFood}>폐기했어요</button>
+          <button className="basic-button bg-primary" onClick={handleEatFood}>
+            맛있게 먹었어요
+          </button>
+          <button
+            className="basic-button bg-primary"
+            onClick={handleDiscardFood}
+          >
+            폐기했어요
+          </button>
         </div>
       </article>
     </main>
