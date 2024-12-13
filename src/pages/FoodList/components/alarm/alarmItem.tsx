9630fbe8c083a48d1c500a9757eb69bda71b2a38
유지원 <jna215843@gmail.com>
2024-12-13 19:14:01 +0900

    feat: 알람 내용 표시. 일정 너비 넘어가면 줄바꿈

--- src/pages/FoodList/components/alarm/alarmItem.tsx
+++ src/pages/FoodList/components/alarm/alarmItem.tsx
@@ -7,7 +7,7 @@ interface ExpiredFood {
 export default function AlarmItem({ item }: { item: ExpiredFood }) {
   return (
     <div className="flex h-[5rem] w-full border-[1px] border-[#E7E7E7] px-[1.2rem] py-[1.4rem] text-[13px]">
-      <span>{item.content}</span>
+      <span className="w-[65%] break-words">{item.content}</span>
 
       <span className="absolute right-[0.9rem] text-[12px] text-gray-500">
         {item.date}
