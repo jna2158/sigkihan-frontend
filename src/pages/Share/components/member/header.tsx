3684304cc00a4b091ad87e3a9909d84870a10704
유지원 <jna215843@gmail.com>
2025-01-03 14:54:35 +0900

    feat: 냉장고 초대하기 기능

--- src/pages/Share/components/member/header.tsx
+++ src/pages/Share/components/member/header.tsx
@@ -1,6 +1,35 @@
+import thumbnail from "../../../../assets/thumbnail.png";
+
 export default function Header() {
-  const handleOpenModal = () => {
-    console.log("open modal");
+  // 친구 초대 버튼 클릭
+  const clickInviteBtn = () => {
+    if (!window.Kakao?.isInitialized()) {
+      window.Kakao?.init(process.env.REACT_APP_KAKAO_JS_SDK_KEY);
+    }
+
+    // 카카오톡 공유하기
+    window.Kakao?.Share.sendDefault({
+      objectType: "feed",
+      content: {
+        title: "냉장고 공유 초대",
+        description: "우리 냉장고를 함께 관리해보세요!",
+        imageUrl: thumbnail,
+        link: {
+          mobileWebUrl: window.location.href,
+          webUrl: window.location.href,
+        },
+      },
+      buttons: [
+        {
+          title: "냉장고 구경하기",
+          link: {
+            mobileWebUrl: window.location.href,
+            webUrl: window.location.href,
+          },
+        },
+      ],
+      installTalk: true,
+    });
   };
 
   return (
@@ -10,7 +39,7 @@ export default function Header() {
       </span>
       <div className="absolute right-[2.1rem] flex gap-4">
         <button
-          onClick={handleOpenModal}
+          onClick={clickInviteBtn}
           className="text-[16px] font-medium text-gray-500"
         >
           친구 초대
